779c4a2f0869627a6ac4e5027c5117ab1e739fc5
jashinreaper <pvqui0910@gmail.com>
2019-12-02 08:21:23 +0700

    them mua hang

---
+++ ltudql_seminar_qlbh/GUI/frmChucNang/frmQuanLyBanHang/frmMuaHang.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ltudql_seminar_qlbh.frmChucNang.frmQuanLyBanHang
+{
+    public partial class frmMuaHang : Form
+    {
+        public frmMuaHang()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
