95924f1857c8d83702dcd7f4a5f68f4a86e294bf
Nguyễn Song Tài <1760170@student.hcmus.edu.vn>
2019-12-03 12:43:16 +0700

    mua hang000

---
+++ ltudql_seminar_qlbh/GUI/frmChucNang/frmQuanLyBanHang/frmBanHang.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace ltudql_seminar_qlbh.frmChucNang.frmQuanLyBanHang
+{
+    public partial class frmBanHang : DevExpress.XtraEditors.XtraForm
+    {
+        public frmBanHang()
+        {
+            InitializeComponent();
+        }
+    }
+}
